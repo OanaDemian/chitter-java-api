5ccfe2f476ef4e820c6d67a88a48c29668e90efd
Oana <oana.demian@gmail.com>
2023-11-25 10:11:41 +0000

    adds user model

---
+++ src/main/java/com/chitter/backend/chitterapi/model/User.java
@@ -0,0 +1,71 @@
+package com.chitter.backend.chitterapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("users")
+public class User {
+
+    @Id
+    @JsonProperty("_id")
+    private String _id;
+
+    @JsonProperty("username")
+    @NotEmpty(message = "Users must have a username")
+    private String username;
+
+    @JsonProperty("name")
+    @NotEmpty(message = "Users must have a name")
+    private String name;
+
+    @JsonProperty("email")
+    @NotEmpty(message = "Users must have an email")
+    private String email;
+
+    @JsonProperty("password")
+    @NotEmpty(message = "Users must have a password")
+    private String password;
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
+
